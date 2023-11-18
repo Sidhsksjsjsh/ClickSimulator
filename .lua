4afaa71d3dd82fa8a69e5a3a77c87e1193f2aedf
Fhri <117529823+Sidhsksjsjsh@users.noreply.github.com>
2023-11-18 12:12:09 +0900

    Update and rename .gitignore to .lua

---
+++ .lua
@@ -0,0 +1,100 @@
+local OrionLib = loadstring(game:HttpGet("https://pastebin.com/raw/NMEHkVTb"))()
+local Window = OrionLib:MakeWindow({Name = "VIP Turtle Hub V3", HidePremium = false, SaveConfig = false, ConfigFolder = "TurtleFi"})
+
+local T1 = Window:MakeTab({
+Name = "Main",
+Icon = "rbxassetid://",
+PremiumOnly = false
+})
+
+local T2 = Window:MakeTab({
+Name = "Egg",
+Icon = "rbxassetid://",
+PremiumOnly = false
+})
+
+local egg = {}
+local zone = {}
+
+OrionLib:AddTable(game:GetService("ReplicatedStorage")["Assets"]["Eggs"],egg)
+OrionLib:AddTable(workspace["Islands"],zone)
+
+T1:AddToggle({
+Name = "Click",
+Default = false,
+Callback = function(Value)
+_G.Click = Value
+      while wait() do
+        if _G.Click == false then break end
+        game:GetService("ReplicatedStorage")["Packages"]["Knit"]["Services"]["ClickService"]["RF"]["Click"]:InvokeServer()
+      end
+end    
+})
+
+T1:AddToggle({
+Name = "Spin",
+Default = false,
+Callback = function(Value)
+_G.spin = Value
+      while wait() do
+        if _G.spin == false then break end
+        game:GetService("ReplicatedStorage")["Packages"]["Knit"]["Services"]["SpinWheelService"]["RF"]["RequestSpin"]:InvokeServer()
+      end
+end    
+})
+
+T1:AddToggle({
+Name = "Rebirth",
+Default = false,
+Callback = function(Value)
+_G.r = Value
+      while wait() do
+        if _G.r == false then break end
+        game:GetService("ReplicatedStorage")["Packages"]["Knit"]["Services"]["RebirthService"]["RF"]["RequestRebirth"]:InvokeServer(game.Players.LocalPlayer)
+      end
+end    
+})
+
+T1:AddToggle({
+Name = "Claim Daily Gifts",
+Default = false,
+Callback = function(Value)
+_G.dg = Value
+      while wait() do
+        if _G.dg == false then break end
+        game:GetService("ReplicatedStorage")["Packages"]["Knit"]["Services"]["GiftsService"]["RF"]["RequestRedeemGift"]:InvokeServer(1)
+        game:GetService("ReplicatedStorage")["Packages"]["Knit"]["Services"]["GiftsService"]["RF"]["RequestRedeemGift"]:InvokeServer(2)
+        game:GetService("ReplicatedStorage")["Packages"]["Knit"]["Services"]["GiftsService"]["RF"]["RequestRedeemGift"]:InvokeServer(3)
+        game:GetService("ReplicatedStorage")["Packages"]["Knit"]["Services"]["GiftsService"]["RF"]["RequestRedeemGift"]:InvokeServer(4)
+        game:GetService("ReplicatedStorage")["Packages"]["Knit"]["Services"]["GiftsService"]["RF"]["RequestRedeemGift"]:InvokeServer(5)
+        game:GetService("ReplicatedStorage")["Packages"]["Knit"]["Services"]["GiftsService"]["RF"]["RequestRedeemGift"]:InvokeServer(6)
+        game:GetService("ReplicatedStorage")["Packages"]["Knit"]["Services"]["GiftsService"]["RF"]["RequestRedeemGift"]:InvokeServer(7)
+        game:GetService("ReplicatedStorage")["Packages"]["Knit"]["Services"]["GiftsService"]["RF"]["RequestRedeemGift"]:InvokeServer(8)
+        game:GetService("ReplicatedStorage")["Packages"]["Knit"]["Services"]["GiftsService"]["RF"]["RequestRedeemGift"]:InvokeServer(9)
+        game:GetService("ReplicatedStorage")["Packages"]["Knit"]["Services"]["GiftsService"]["RF"]["RequestRedeemGift"]:InvokeServer(10)
+        game:GetService("ReplicatedStorage")["Packages"]["Knit"]["Services"]["GiftsService"]["RF"]["RequestRedeemGift"]:InvokeServer(11)
+        game:GetService("ReplicatedStorage")["Packages"]["Knit"]["Services"]["GiftsService"]["RF"]["RequestRedeemGift"]:InvokeServer(12)
+      end
+end    
+})
+
+T2:AddDropdown({
+  Name = "Select Egg",
+  Default = egg[1],
+  Options = egg,
+  Callback = function(Value)
+   _G.EggName = Value
+  end    
+})
+
+T2:AddToggle({
+Name = "Hatch",
+Default = false,
+Callback = function(Value)
+_G.ht = Value
+      while wait() do
+        if _G.ht == false then break end
+        game:GetService("ReplicatedStorage")["Packages"]["Knit"]["Services"]["EggService"]["RF"]["OpenEgg"]:InvokeServer(_G.EggName,"Single")
+      end
+end    
+})
